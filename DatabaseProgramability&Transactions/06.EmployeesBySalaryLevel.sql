a31e15058a3cb87d71087540abf89d430df35d9f
Kiril Simeonov <45626567+kirilsimeonov@users.noreply.github.com>
2020-03-11 11:39:03 +0200

    Employees by Salary Level

---
+++ DatabaseProgramability&Transactions/06.EmployeesBySalaryLevel.sql
@@ -0,0 +1,6 @@
+CREATE PROC usp_EmployeesBySalaryLevel(@salaryLevel VARCHAR(10))
+AS
+	SELECT e.FirstName AS [First Name],
+	       e.LastName AS [Last Name]
+	  FROM Employees AS e
+	 WHERE dbo.ufn_GetSalaryLevel(e.Salary) = @salaryLevel
